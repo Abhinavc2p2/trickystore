7ad0d3b2037ea64e51c150acd084cfdfa41ae3bb
Abhinav  Mishra <abhi77385@gmail.com>
2025-01-10 10:48:26 +0530

    admin done

---
+++ client/src/pages/user/Profile.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Layout from '../../components/Layout/Layout'
+import UserMenu from '../../components/Layout/UserMenu'
+
+const Profile = () => {
+  return (
+   <>
+   <Layout title={"User-profile"}>
+   <div className="container-fluid m-3 p-3">
+    <div className='row'>
+    <div className='col-md-3'><UserMenu></UserMenu>
+    </div>
+    <div className='col-md-9'>
+        <h1>
+        Your Profile
+        </h1>
+    </div>
+    </div>
+    </div>
+   </Layout>
+   </>
+  )
+}
+
+export default Profile
