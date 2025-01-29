f469bd71546b55746f814b893c0f1adcd1f8d3e0
Abhinav  Mishra <abhi77385@gmail.com>
2025-01-29 18:17:07 +0530

    done

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import productroute from "./routes/ProductRoutes.js";
 import categoryRoute from "./routes/categoryRoute.js"
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path"
 
 
 
@@ -23,6 +24,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname,'./client/build')))
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -30,9 +32,9 @@ app.use("/api/v1/category",categoryRoute)
 app.use("/api/v1/Product",productroute)
 
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
-});
+app.use('*',function(req,res){
+  res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
 
 //PORT
 const PORT = process.env.PORT || 8080;
